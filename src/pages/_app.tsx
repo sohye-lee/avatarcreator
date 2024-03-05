b4bfd240f913a7f7135af055fe67065b484e65e7
Sohye Lee <sohyelee@gmail.com>
2024-03-05 17:28:55 -0500

    setting and first section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import { Inter, Gayathri } from "next/font/google";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
-const inter = Inter({
+// const inter = Gayathri({
+//   // subsets: ["latin"],
+//   // variable: "--font-sans",
+// });
+
+const inter = Gayathri({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: "100",
 });
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import Header from "@/components/header";
 import React from "react";
+import BG from "@public/images/background.jpg";
+import Image from "next/image";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className=" min-h-screen w-full ">
+      <Header />
+      <div className="relative flex w-full flex-col items-center  ">
+        <Image
+          fill
+          src={BG}
+          alt="background"
+          className="absolute left-0 top-0 z-10 w-full"
+        />
+        <div className="relative z-20 flex flex-col items-center space-y-5 py-32">
+          <h1 className="relative z-20  text-center text-5xl font-medium">
+            Transform Your Selfies
+            <br />
+            into Unique AI Avatars
+          </h1>
+          <p className="text-md text-center text-slate-500">
+            From Snapshots to Avatars, Effortlessly.
+          </p>
+          <button className="rounded-full bg-black from-fuchsia-500 to-cyan-500 px-8 py-2 text-white hover:bg-gradient-to-r">
+            Create Your Avatar with AI
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
